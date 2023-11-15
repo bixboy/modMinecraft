36a1ed2a13d1d16fa5d5acb7cc0366f37637c3d1
pierre <pierrelamare62@gmail.com>
2023-11-15 15:10:48 +0100

    Auto stash before merge of "master" and "origin/master"

--- forge-1.16.5-36.2.34-mdk/src/main/java/fr/bixboy/csm/gui/playerSize/PlayerSizeGUI.java
+++ forge-1.16.5-36.2.34-mdk/src/main/java/fr/bixboy/csm/gui/playerSize/PlayerSizeGUI.java
@@ -1,8 +1,10 @@
 package fr.bixboy.csm.gui.playerSize;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import fr.bixboy.csm.CSM;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
 
 public class PlayerSizeGUI extends Screen{
@@ -10,38 +12,31 @@ public class PlayerSizeGUI extends Screen{
         super(new StringTextComponent("Player Size GUI"));
     }
 
+    private final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(CSM.MODID, "textures/ui_background.png");
+    private int xSize = 256;
+    private int ySize = 202;
+
+    private int guiLeft;
+    private int guiTop;
     @Override
     protected void init() {
-        // Définir la largeur et la hauteur de l'écran
-        int width = this.width;
-        int height = this.height;
+        this.guiLeft = (this.width - this.xSize) / 2;
+        this.guiTop = (this.height - this.ySize) / 2;
 
-        // Position du fond blanc au centre de l'écran
-        int centerX = width / 2 - 100;
-        int centerY = height / 2 - 100;
-
-        // Dessiner un fond blanc au centre de l'écran
-        fill(centerX, centerY, centerX + 200, centerY + 200, 0xFFFFFFFF); // Remplacez la couleur par celle que vous souhaitez
-
-        // Dessiner une barre latérale à gauche avec quatre boutons
-        int sidebarWidth = 50;
-        int buttonHeight = 40;
-        int gap = 10;
-        int startY = centerY + gap; // Commencer la barre latérale un peu en bas du fond blanc
-        int startX = centerX - sidebarWidth - gap; // Positionner la barre latérale à gauche du fond blanc
-
-        for (int i = 0; i < 4; i++) {
-            int buttonY = startY + i * (buttonHeight + gap);
-
-            // Dessiner chaque bouton dans la barre latérale
-            addButton(new ButtonRaces(startX, buttonY, sidebarWidth, buttonHeight, i));
-        }
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        drawBackground(matrices);
+
         this.renderBackground(matrices);
         super.render(matrices, mouseX, mouseY, delta);
+    }
+
+    private void drawBackground(MatrixStack matrices)
+    {
+        this.minecraft.getTextureManager().bind(TEXTURE_LOCATION);
+        this.blit(matrices, guiLeft, guiTop, 0, 0, this.xSize, this.ySize);
     }
 
     @Override
