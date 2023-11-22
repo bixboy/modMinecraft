483cbeee8553934a7425f82eb23f8cd6fb443299
Bixboy <pierrelamare62@gmail.com>
2023-11-22 23:15:59 +0100

    oui

--- forge-1.16.5-36.2.34-mdk/src/main/java/fr/bixboy/csm/gui/playerSize/PlayerSizeGUI.java
+++ forge-1.16.5-36.2.34-mdk/src/main/java/fr/bixboy/csm/gui/playerSize/PlayerSizeGUI.java
@@ -8,6 +8,7 @@ import fr.bixboy.csm.entity.model.CustomPlayerModel;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.DisplayEffectsScreen;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.gui.widget.button.ImageButton;
@@ -26,29 +27,28 @@ import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PlayerSizeGUI extends Screen {
+public class PlayerSizeGUI extends Screen{
     public PlayerSizeGUI() {
         super(new StringTextComponent("Player Size GUI"));
     }
 
     private final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(CSM.MODID, "textures/ui_background.png");
 
-    private int xSize = 256;
-    private int ySize = 202;
+    private int xSize = 174;
+    private int ySize = 230;
 
     private int guiLeft;
     private int guiTop;
 
-    private int race;
-    private String lang_races;
-    private String desc_races;
+    private int race = 1;
+    static String lang_races;
+    static String desc_races;
 
     private float xMouse;
     private float yMouse;
-
-    private float MAX_SIZE = 1f;
-
     private boolean ScreenInf = true;
     private boolean ScreenRace;
     private boolean ScreenAccessory;
@@ -56,8 +56,6 @@ public class PlayerSizeGUI extends Screen {
     private boolean Skins;
 
     private static final ResourceLocation BUTTON_IMAGE1 = new ResourceLocation(CSM.MODID, "textures/bg_gui_base.png");
-    private static final ResourceLocation BUTTON_IMAGE2 = new ResourceLocation(CSM.MODID, "textures/baguette-magique.png");
-    private static final ResourceLocation BUTTON_IMAGE3 = new ResourceLocation(CSM.MODID, "textures/portrait.png");
 
     private CustomPlayerModel customPlayerModel; // Instance de votre modèle de joueur personnalisé
 
@@ -83,37 +81,71 @@ public class PlayerSizeGUI extends Screen {
         this.guiLeft = (this.width - this.xSize) / 2;
         this.guiTop = (this.height - this.ySize) / 2;
 
-        this.addButton(new ImageButton(guiLeft + (xSize / 2) + 80, guiTop + (ySize / 2) - 50, 21, 22, 174, 44, 0, BUTTON_IMAGE1, humanButton -> {
+        this.addButton(new ImageButton(guiLeft + 165, guiTop + (ySize / 2) - 50, 21, 22, 174, 44, 0, BUTTON_IMAGE1, humanButton -> {
             ScreenInf = true;
             ScreenRace = false;
             ScreenAccessory = false;
             Skins = false;
         }));
 
-        this.addButton(new ImageButton(guiLeft + (xSize/2) + 80,  guiTop + (ySize/2) - 20, 21, 22, 174, 44, 0, BUTTON_IMAGE1, humanButton -> {
+        this.addButton(new ImageButton(guiLeft + 165,  guiTop + (ySize/2) - 20, 21, 22, 174, 44, 0, BUTTON_IMAGE1, humanButton -> {
             ScreenRace = true;
             ScreenAccessory = false;
             ScreenInf = false;
             Skins = false;
         }));
 
-        this.addButton(new ImageButton(guiLeft + (xSize/2) + 80,  guiTop + (ySize/2) + 10, 21, 22, 174, 66, 0, BUTTON_IMAGE1, humanButton -> {
+        this.addButton(new ImageButton(guiLeft + 165,  guiTop + (ySize/2) + 10, 21, 22, 174, 66, 0, BUTTON_IMAGE1, humanButton -> {
             ScreenAccessory = true;
             ScreenRace = false;
             ScreenInf = false;
             Skins = false;
         }));
 
-        this.addButton(new ImageButton(guiLeft + (xSize/2) + 80,  guiTop + (ySize/2) + 40, 21, 22, 174, 66, 0, BUTTON_IMAGE1, humanButton -> {
+        this.addButton(new ImageButton(guiLeft + 165,  guiTop + (ySize/2) + 40, 21, 22, 174, 66, 0, BUTTON_IMAGE1, humanButton -> {
             Skins = true;
             ScreenAccessory = false;
             ScreenRace = false;
             ScreenInf = false;
         }));
 
-        this.addButton(new Button(guiLeft + (xSize/2),  guiTop + (ySize/2) + 70, 60, 20, new TranslationTextComponent("Save"), saveButton -> {
+        this.addButton(new Button(guiLeft + 58,  guiTop + 215, 60, 20, new TranslationTextComponent("Save"), saveButton -> {
             this.minecraft.setScreen((Screen)null);
             this.minecraft.mouseHandler.grabMouse();
+        }));
+
+        //Races
+
+        this.addButton(new ImageButton(guiLeft - 60,  guiTop + 65, 69, 16, 174, 169, 0, BUTTON_IMAGE1, humanButton -> {
+            Skins = false;
+            ScreenAccessory = false;
+            ScreenRace = false;
+            ScreenInf = true;
+            race = 1;
+        }));
+
+        this.addButton(new ImageButton(guiLeft - 60,  guiTop + 85, 69, 16, 174, 169, 0, BUTTON_IMAGE1, DwarvesButton -> {
+            Skins = false;
+            ScreenAccessory = false;
+            ScreenRace = false;
+            ScreenInf = true;
+            race = 2;
+        }));
+
+        this.addButton(new ImageButton(guiLeft - 60,  guiTop + 105, 69, 16, 174, 169, 0, BUTTON_IMAGE1, ElvesButton -> {
+            Skins = false;
+            ScreenAccessory = false;
+            ScreenRace = false;
+            ScreenInf = true;
+            race = 3;
+        }));
+
+        this.addButton(new ImageButton(guiLeft - 60,  guiTop + 125, 69, 16, 174, 169, 0, BUTTON_IMAGE1, KullButton -> {
+            Skins = false;
+            ScreenAccessory = false;
+            ScreenRace = false;
+            ScreenInf = true;
+            race = 4;
         }));
     }
 
@@ -131,9 +163,9 @@ public class PlayerSizeGUI extends Screen {
 
     private void drawBackground(MatrixStack matrices)
     {
-        this.minecraft.getTextureManager().bind(TEXTURE_LOCATION);
-        this.blit(matrices, guiLeft, guiTop, 0, 0, this.xSize, this.ySize);
-        renderEntityInInventory(guiLeft + 51, guiTop + 75, 30, (float)(guiLeft + 51) - this.xMouse, (float)(guiTop + 75 - 50) - this.yMouse, this.minecraft.player);
+        this.minecraft.getTextureManager().bind(BUTTON_IMAGE1);
+        this.blit(matrices, guiLeft, guiTop, 0, 0, 174, 230);
+        renderEntityInInventory(guiLeft + 87, guiTop + 120, 40, (float)(guiLeft + 51) - this.xMouse, (float)(guiTop + 75 - 50) - this.yMouse, this.minecraft.player);
     }
 
 
@@ -143,34 +175,114 @@ public class PlayerSizeGUI extends Screen {
     }
 
     public void drawText(MatrixStack matrixStack) {
-        if (ScreenInf && !ScreenAccessory && !ScreenRace)
+        if (ScreenInf && !ScreenAccessory && !ScreenRace && !Skins)
         {
-            drawCenteredString(matrixStack, this.font, "Chose your Race", guiLeft + 128, guiTop + 20, 0xFFFFFF);
+            drawCenteredString(matrixStack, this.font, "Chose your Race", guiLeft + 87, guiTop + 20, 11299672);
 
-        } else if (ScreenRace && !ScreenAccessory && !ScreenInf)
+        } else if (ScreenRace && !ScreenAccessory && !ScreenInf && !Skins)
         {
-            drawCenteredString(matrixStack, this.font, "Chose your Gender", guiLeft + 128, guiTop + 20, 0xFFFFFF);
+            drawCenteredString(matrixStack, this.font, "Chose your Gender", guiLeft + 87, guiTop + 20, 11299672);
 
-        } else if (ScreenAccessory && !ScreenInf && !ScreenRace)
+        } else if (ScreenAccessory && !ScreenInf && !ScreenRace && !Skins)
         {
-            drawCenteredString(matrixStack, this.font, "Chose your Accessory", guiLeft + 128, guiTop + 20, 0xFFFFFF);
+            drawCenteredString(matrixStack, this.font, "Chose your Accessory", guiLeft + 87, guiTop + 20, 11299672);
         } else if (Skins && !ScreenAccessory && !ScreenInf && !ScreenRace)
         {
-            drawCenteredString(matrixStack, this.font, "Chose your Skin", guiLeft + 128, guiTop + 20, 0xFFFFFF);
+            drawCenteredString(matrixStack, this.font, "Chose your Skin", guiLeft + 87, guiTop + 20, 11299672);
         }
 
         switch (race) {
             case 1:
-                lang_races = "gui.race_tipo_1";
-                desc_races = "gui.description_human";
-                drawCenteredString(matrixStack, I18n.exists("gui.race_tipo_1", new Object[0]), this.xSize - guiLeft / 2 - this.field_146289_q.func_78256_a(I18n.func_135052_a("gui.razza_tipo_1", new Object[0])) / 2 + 61, this.ySize + 34, 16644329);
-                drawCenteredString(matrixStack, I18n.get("gui.race_tipo_2", new Object[0]), this.xSize - guiLeft / 2 - this.field_146289_q.func_78256_a(I18n.func_135052_a("gui.razza_tipo_2", new Object[0])) / 2 + 61, this.ySize + 51, 11299672);
-                drawCenteredString(matrixStack, I18n.get("gui.race_tipo_3", new Object[0]), this.xSize - guiLeft / 2 - this.field_146289_q.func_78256_a(I18n.func_135052_a("gui.razza_tipo_3", new Object[0])) / 2 + 61, this.ySize + 68, 11299672);
-                drawCenteredString(matrixStack, I18n.get("gui.race_tipo_4", new Object[0]), this.xSize - guiLeft / 2 - this.field_146289_q.func_78256_a(I18n.func_135052_a("gui.razza_tipo_4", new Object[0])) / 2 + 61, this.ySize + 85, 11299672);
-                drawCenteredString(matrixStack, I18n.get("gui.race_tipo_5", new Object[0]), this.xSize - guiLeft / 2 - this.field_146289_q.func_78256_a(I18n.func_135052_a("gui.razza_tipo_5", new Object[0])) / 2 + 61, this.ySize + 102, 11299672);
-                drawCenteredString(matrixStack, I18n.get("gui.race_tipo_6", new Object[0]), this.xSize - guiLeft / 2 - this.field_146289_q.func_78256_a(I18n.func_135052_a("gui.razza_tipo_6", new Object[0])) / 2 + 61, this.ySize + 119, 11299672);
-                drawCenteredString(matrixStack, I18n.get("gui.race_tipo_7", new Object[0]), this.xSize - guiLeft / 2 - this.field_146289_q.func_78256_a(I18n.func_135052_a("gui.razza_tipo_7", new Object[0])) / 2 + 61, this.ySize + 136, 11299672);
+                lang_races = I18n.get("gui.race_tipo_1");
+                desc_races = I18n.get("gui.description_human");
+
+                if (!Skins && !ScreenAccessory && ScreenInf && !ScreenRace) {
+                    renderTextWithLineBreaks(matrixStack, desc_races, guiLeft + 75, guiTop + 150, 11299672, 27);
+                }
+
+                drawCenteredString(matrixStack, this.font, lang_races, guiLeft + (xSize/2) - 95, guiTop + 70, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_2"), guiLeft + (xSize/2) - 95, guiTop + 90, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_3"), guiLeft + (xSize/2) - 95, guiTop + 110, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_4"), guiLeft + (xSize/2) - 95, guiTop + 130, 11299672);
+
                 break;
+
+            case 2:
+                lang_races = I18n.get("gui.race_tipo_2");
+                desc_races = I18n.get("gui.description_dwarves");
+
+                if (!Skins && !ScreenAccessory && ScreenInf && !ScreenRace) {
+                    renderTextWithLineBreaks(matrixStack, desc_races, guiLeft + 75, guiTop + 140, 11299672, 27);
+                }
+                drawCenteredString(matrixStack, this.font, lang_races, guiLeft + (xSize/2) - 95, guiTop + 90, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_1"), guiLeft + (xSize/2) - 95, guiTop + 70, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_3"), guiLeft + (xSize/2) - 95, guiTop + 110, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_4"), guiLeft + (xSize/2) - 95, guiTop + 130, 11299672);
+
+                break;
+            case 3:
+                lang_races = I18n.get("gui.race_tipo_3");
+                desc_races = I18n.get("gui.description_elves");
+
+                if (!Skins && !ScreenAccessory && ScreenInf && !ScreenRace) {
+                    renderTextWithLineBreaks(matrixStack, desc_races, guiLeft + 75, guiTop + 140, 11299672, 27);
+                }
+                drawCenteredString(matrixStack, this.font, lang_races, guiLeft + (xSize/2) - 95, guiTop + 110, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_2"),  guiLeft+ (xSize/2) - 95, guiTop + 90, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_1"), guiLeft + (xSize/2) - 95, guiTop + 70, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_4"), guiLeft + (xSize/2) - 95, guiTop + 130, 11299672);
+
+                break;
+            case 4:
+                lang_races = I18n.get("gui.race_tipo_4");
+                desc_races = I18n.get("gui.description_kull");
+
+                if (!Skins && !ScreenAccessory && ScreenInf && !ScreenRace) {
+                    renderTextWithLineBreaks(matrixStack, desc_races, guiLeft + 75, guiTop + 150, 11299672, 27);
+                }
+                drawCenteredString(matrixStack, this.font, lang_races, guiLeft + (xSize/2) - 95, guiTop + 130, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_2"), guiLeft + (xSize/2) - 95, guiTop + 90, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_3"), guiLeft + (xSize/2) - 95, guiTop + 110, 11299672);
+                drawCenteredString(matrixStack, this.font, I18n.get("gui.race_tipo_1"), guiLeft + (xSize/2) - 95, guiTop + 70, 11299672);
+
+                break;
+            case 5:
+                lang_races = I18n.get("gui.race_tipo_5");
+                desc_races = I18n.get("gui.description_human");
+                renderTextWithLineBreaks(matrixStack, desc_races, guiLeft + 75, guiTop + 150, 11299672, 27);
+
+                break;
+        }
+
+    }
+
+    private void renderTextWithLineBreaks(MatrixStack matrixStack, String text, int x, int y, int color, int maxWidth) {
+        List<String> lines = new ArrayList<>();
+        int index = 0;
+        while (index < text.length()) {
+            int endIndex = Math.min(index + maxWidth, text.length());
+            String line = text.substring(index, endIndex);
+            int lineBreakIndex = line.lastIndexOf("2" );
+            if (lineBreakIndex != -1 && lineBreakIndex < endIndex - index) {
+                endIndex = index + lineBreakIndex + 1;
+                line = text.substring(index, endIndex);
+            }
+            lines.add(line.trim()); // Ajouter la ligne sans les espaces en fin de ligne
+            index = endIndex;
+        }
+
+        int lineHeight = this.font.lineHeight;
+        int totalTextHeight = lines.size() * lineHeight;
+        int yOffset = 0;
+
+        for (String line : lines) {
+            int textWidth = this.font.width(line);
+            int xOffset = (maxWidth - textWidth) / 2; // Calcul du décalage pour centrer le texte horizontalement
+            if (textWidth < maxWidth) {
+                xOffset = 0; // Ne pas centrer si la largeur du texte est inférieure à la largeur maximale
+            }
+            this.font.draw(matrixStack, line, x + xOffset, y + yOffset, color);
+            yOffset += lineHeight;
         }
     }
 
